6a4ab81da4a421d027d2ab1c8bbf9291bd6a6054
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-14 16:09:32 -0300

    feat: make entity manager

---
+++ src/Utils/EntityManager.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SpaceInvadersRetro.Interfaces;
+
+namespace SpaceInvadersRetro.Utils;
+
+public abstract class EntityManager
+{
+    public static List<IEntity> Entities { get; set; } = new();
+
+    public static void LoadContent(ContentManager content)
+    {
+        foreach (var entity in Entities)
+        {
+            entity.LoadContent(content);
+        }
+    }
+
+    public static void Update(GameTime gameTime)
+    {
+        foreach (var entity in Entities)
+        {
+            entity.Update(gameTime);
+        }
+    }
+
+    public static void Draw(SpriteBatch spriteBatch)
+    {
+        foreach (var entity in Entities)
+        {
+            entity.Draw(spriteBatch);
+        }
+    }
+}
