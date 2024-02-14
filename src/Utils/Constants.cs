c19de2cb3a11e3a83c7ce6944d12f020bff879e3
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-14 15:14:14 -0300

    feat: make constants for margin and screen size

---
+++ src/Utils/Constants.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace SpaceInvadersRetro.Utils;
+
+public readonly struct MARGIN
+{
+    public static readonly Dictionary<string, int> X = new() { { "min", 20 }, { "max", 96 } };
+    public static readonly Dictionary<string, int> Y = new() { { "top", 200 }, { "bottom", 38 } };
+}
+
+public readonly struct SCREEN
+{
+    public static readonly int WIDTH = 720;
+    public static readonly int HEIGHT = 896;
+}
