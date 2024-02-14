6bbd420822db2db892faa6460d7bf8ea0de753bd
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-14 13:49:20 -0300

    feat: make game screen

---
+++ src/Screens/GameScreen.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceInvadersRetro.Screens;
+
+public class GameScreen : IBaseScreen
+{
+    public void Draw(SpriteBatch spriteBatch) { }
+
+    public void Initialize() { }
+
+    public void LoadContent() { }
+
+    public void Update(GameTime gameTime) { }
+}
