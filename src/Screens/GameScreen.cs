b182bb4ef8e185280f61139899be1c359f150971
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-14 15:37:06 -0300

    refactor: change margin bottom in Constants.cs and load spaceship in GameScreen.cs

--- src/Screens/GameScreen.cs
+++ src/Screens/GameScreen.cs
@@ -2,6 +2,8 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
+using SpaceInvadersRetro.Components;
+using SpaceInvadersRetro.Utils;
 
 namespace SpaceInvadersRetro.Screens;
 
@@ -10,27 +12,37 @@ public class GameScreen : IBaseScreen
     private readonly GraphicsDeviceManager _graphicsDevice;
     private Texture2D _background;
     private Song _music;
+    private Spaceship _spaceship;
 
     public GameScreen(GraphicsDeviceManager graphics)
     {
         _graphicsDevice = graphics;
     }
 
-    public void Initialize() { }
+    public void Initialize()
+    {
+        var spaceshipPosition = new Vector2(SCREEN.WIDTH / 2, SCREEN.HEIGHT - MARGIN.Y["bottom"]);
+        _spaceship = new Spaceship(spaceshipPosition);
+    }
 
     public void LoadContent(ContentManager content)
     {
         _background = content.Load<Texture2D>("Images/Background");
         _music = content.Load<Song>("Sounds/spaceinvadersmusic");
+        _spaceship.LoadContent(content);
 
         MediaPlayer.Volume -= .7f;
         MediaPlayer.Play(_music);
     }
 
-    public void Update(GameTime gameTime) { }
+    public void Update(GameTime gameTime)
+    {
+        _spaceship.Update(gameTime);
+    }
 
     public void Draw(SpriteBatch spriteBatch)
     {
         spriteBatch.Draw(_background, new Vector2(0, 0), Color.White);
+        _spaceship.Draw(spriteBatch);
     }
 }
--- src/Utils/Constants.cs
+++ src/Utils/Constants.cs
@@ -5,7 +5,7 @@ namespace SpaceInvadersRetro.Utils;
 public readonly struct MARGIN
 {
     public static readonly Dictionary<string, int> X = new() { { "min", 20 }, { "max", 96 } };
-    public static readonly Dictionary<string, int> Y = new() { { "top", 200 }, { "bottom", 38 } };
+    public static readonly Dictionary<string, int> Y = new() { { "top", 200 }, { "bottom", 82 } };
 }
 
 public readonly struct SCREEN
