cc80f21eff0f34661004e0723cc37c0ab477323a
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-15 00:34:01 -0300

    refactor: extends AlienBase in BonusShip

--- src/Components/Aliens/BonusShip.cs
+++ src/Components/Aliens/BonusShip.cs
@@ -2,21 +2,32 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using SpaceInvadersRetro.Interfaces;
 using SpaceInvadersRetro.Utils;
 
 namespace SpaceInvadersRetro.Components;
 
-public class BonusShip : IEntity
+public class BonusShip : AlienBase
 {
     private Vector2 _position;
     private Vector2 _initialPosition;
     private Rectangle _bounds;
-    public Texture2D Texture { get; set; }
-    public int Points { get; set; }
+
     public bool OnScreen { get; set; } = false;
     public float Speed { get; set; } = 100f;
     public double TimeToEnter { get; set; }
+    public override bool IsAlive { get; set; } = true;
+    public override Texture2D Texture { get; set; }
+    public override int Points { get; set; }
+    public override Vector2 Position
+    {
+        get => _position;
+        set => _position = value;
+    }
+    public override Rectangle Bounds
+    {
+        get => _bounds;
+        set => _bounds = value;
+    }
 
     public BonusShip()
     {
@@ -30,12 +41,12 @@ public class BonusShip : IEntity
         Points = RandomManager.GetRandomPoints();
     }
 
-    public void LoadContent(ContentManager content)
+    public override void LoadContent(ContentManager content)
     {
         Texture = content.Load<Texture2D>("Sprites/Aliens/Soraka");
     }
 
-    public void Update(GameTime gameTime)
+    public override void Update(GameTime gameTime)
     {
         bool onScreen = _position.X >= -Texture.Width;
 
@@ -57,5 +68,6 @@ public class BonusShip : IEntity
         _bounds.X -= (int)toLeft;
     }
 
-    public void Draw(SpriteBatch spriteBatch) => spriteBatch.Draw(Texture, _position, Color.White);
+    public override void Draw(SpriteBatch spriteBatch) =>
+        spriteBatch.Draw(Texture, _position, Color.White);
 }
