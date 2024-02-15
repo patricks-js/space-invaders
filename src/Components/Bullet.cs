10ab00e7a8c7574d589ac6f1528dbf29c2bb0a33
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-15 11:50:09 -0300

    refactor: improve bullet move direction

--- src/Components/Bullet.cs
+++ src/Components/Bullet.cs
@@ -1,40 +1,33 @@
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using SpaceInvadersRetro.Interfaces;
 using SpaceInvadersRetro.Utils;
 
 namespace SpaceInvadersRetro.Components;
 
 public class Bullet
 {
+    private Vector2 _direction;
     public Texture2D Texture { get; set; }
     public Vector2 _position;
     public Rectangle _bounds;
     public float Speed { get; set; } = 500f;
 
-    public Bullet(Vector2 initialPosition, Texture2D texture)
+    public Bullet(Vector2 initialPosition, Texture2D texture, Vector2 direction)
     {
         Texture = texture;
         _position = initialPosition;
+        _direction = direction;
         _bounds = new((int)_position.X, (int)_position.Y, Texture.Width, Texture.Height);
     }
 
-    public void GoUp(GameTime gameTime)
+    public void Update(GameTime gameTime)
     {
-        var bulletUp = Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        var bulletUp = _direction * Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-        _position.Y -= bulletUp;
+        _position += bulletUp;
 
-        _bounds.Y -= (int)bulletUp;
-    }
-
-    public void GoDown(GameTime gameTime)
-    {
-        var bulletUp = Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-        _position.Y += bulletUp;
-
-        _bounds.Y += (int)bulletUp;
+        _bounds.Location += bulletUp.ToPoint();
     }
 
     public void Draw(SpriteBatch spriteBatch) => spriteBatch.Draw(Texture, _position, Color.White);
