5a8d84aab725109a49f13249ca762f323bf2d177
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-18 00:08:42 -0300

    refactor(AlienBase): remove unused methods

--- src/Components/Aliens/AlienBase.cs
+++ src/Components/Aliens/AlienBase.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using SpaceInvadersRetro.Interfaces;
+using SpaceInvadersRetro.Utils;
 
 namespace SpaceInvadersRetro.Components;
 
@@ -11,6 +12,4 @@ public abstract class AlienBase : EntityBase
     public abstract Vector2 Position { get; set; }
     public abstract bool IsAlive { get; set; }
     public abstract int Points { get; set; }
-
-    public abstract void Explosion();
 }
