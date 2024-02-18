287d1361e98979b94d24f6548195432742ac2896
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-18 00:40:37 -0300

    feat: make a score manager

---
+++ src/Utils/ScoreManager.cs
@@ -0,0 +1,11 @@
+namespace SpaceInvadersRetro.Utils;
+
+public class ScoreManager
+{
+    public static int Score { get; private set; }
+
+    public static void Increment(int points)
+    {
+        Score += points;
+    }
+}
