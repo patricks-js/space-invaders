5473019813070bee267972785cf9744050923f50
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-19 15:02:39 -0300

    feat(Carry): implement shootable interface

--- src/Components/Aliens/Carry.cs
+++ src/Components/Aliens/Carry.cs
@@ -6,10 +6,13 @@ using SpaceInvadersRetro.Utils;
 
 namespace SpaceInvadersRetro.Components.Aliens;
 
-public class Carry : AlienBase
+public class Carry : AlienBase, IShootable
 {
     private Vector2 _position;
     private Rectangle _bounds;
+    private Bullet _bullet;
+    private Texture2D _bulletTexture;
+    private double _timeToShot;
 
     public override Texture2D Texture { get; set; }
     public override bool IsAlive { get; set; } = true;
@@ -29,11 +32,13 @@ public class Carry : AlienBase
     {
         _position = position;
         _sprites = new Rectangle[SPRITE_FRAMES.ALIENS];
+        _timeToShot = RandomManager.Random.Next(1, 10) * 10 / 2;
     }
 
     public override void LoadContent(ContentManager content)
     {
         Texture = content.Load<Texture2D>("Sprites/Aliens/Carry");
+        _bulletTexture = content.Load<Texture2D>("Sprites/AlienBullet");
 
         var w = SPRITE_SIZE.ALIENS["width"];
         var h = SPRITE_SIZE.ALIENS["height"];
@@ -45,6 +50,7 @@ public class Carry : AlienBase
     public override void Update(GameTime gameTime)
     {
         _elapsedTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+        _timeToShot -= (float)gameTime.ElapsedGameTime.TotalSeconds;
 
         var x = (int)_position.X;
         var y = (int)_position.Y;
@@ -54,6 +60,20 @@ public class Carry : AlienBase
         Animation();
 
         _bounds = new(x, y, w, h);
+
+        if (_timeToShot <= 0)
+        {
+            Shoot();
+            _timeToShot = RandomManager.Random.Next(2, 6) * 10;
+        }
+
+        if (_bullet != null && _bullet.IsOffScreen())
+        {
+            BulletManager.RemoveBullet(_bullet);
+            _bullet = null;
+        }
+
+        CheckCollision();
     }
 
     public override void Draw(SpriteBatch spriteBatch)
@@ -66,5 +86,28 @@ public class Carry : AlienBase
         IsAlive = false;
         ScoreManager.Increment(Points);
         EntityManager.RemoveEntity(this);
+    }
+
+    public void Shoot()
+    {
+        var bulletPos = new Vector2(
+            Position.X + (Texture.Width - _bulletTexture.Width) / 2,
+            Position.Y
+        );
+
+        _bullet = BulletManager.CreateBullet(bulletPos, _bulletTexture, Vector2.UnitY, 350f);
+    }
+
+    public void CheckCollision()
+    {
+        foreach (var e in EntityManager.Entities)
+        {
+            if (_bullet != null && _bullet._bounds.Intersects(e.Bounds) && e != this)
+            {
+                e.HandleCollision();
+                BulletManager.RemoveBullet(_bullet);
+                _bullet = null;
+            }
+        }
     }
 }
