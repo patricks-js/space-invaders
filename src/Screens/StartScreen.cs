22705644913eb4e6075e15da86eed81b27aa52b7
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-19 11:41:30 -0300

    feat: add logo on start screen

--- src/Screens/StartScreen.cs
+++ src/Screens/StartScreen.cs
@@ -3,15 +3,21 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SpaceInvadersRetro.Interfaces;
-using SpaceInvadersRetro.Screens;
 using SpaceInvadersRetro.Utils;
+
+namespace SpaceInvadersRetro.Screens;
 
 public class StartScreen : IBaseScreen
 {
+    private SpaceInvadersGame _game;
     private Texture2D _background;
+    private Texture2D _logo;
     private Texture2D _selectStart;
 
-    //private int choice = 0;
+    public StartScreen(SpaceInvadersGame game)
+    {
+        _game = game;
+    }
 
     public void Initialize()
     {
@@ -22,13 +28,8 @@ public class StartScreen : IBaseScreen
     public void LoadContent(ContentManager content)
     {
         _background = content.Load<Texture2D>("Images/Background");
+        _logo = content.Load<Texture2D>("Images/Logo");
         _selectStart = content.Load<Texture2D>("Images/SelectStart");
-    }
-
-    public void Draw(SpriteBatch spriteBatch)
-    {
-        spriteBatch.Draw(_background, new Vector2(0, 0), Color.White);
-        spriteBatch.Draw(_selectStart, new Vector2(200, 350), Color.White);
     }
 
     public void Update(GameTime gameTime)
@@ -37,7 +38,18 @@ public class StartScreen : IBaseScreen
 
         if (keyboard.IsKeyDown(Keys.Space))
         {
-            ScreenManager.Change(new GameScreen());
+            _game.ChangeScreen(new GameScreen());
         }
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_background, new Vector2(0, 0), Color.White);
+        spriteBatch.Draw(
+            _logo,
+            new Vector2(SCREEN.WIDTH / 2 - _logo.Width / 2, MARGIN.Y["bottom"]),
+            Color.White
+        );
+        spriteBatch.Draw(_selectStart, new Vector2(200, 350), Color.White);
     }
 }
