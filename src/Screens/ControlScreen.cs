3c0cbb5bddcb38bb7f2a766c9129e16fc19b7b3c
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-19 11:16:24 -0300

    fix(ControlSCreen): solve namespace problems

--- src/Screens/ControlScreen.cs
+++ src/Screens/ControlScreen.cs
@@ -1,7 +1,9 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using SpaceInvadersRetro.Screens;
+using SpaceInvadersRetro.Interfaces;
+
+namespace SpaceInvadersRetro.Screens;
 
 public class ControlScreen : IBaseScreen
 {
