dfad7ca1d4c1f0a3bc8a5dbb490f728cab217331
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-02-25 13:58:14 -0300

    refactor(AlienBase): improve fields semantic

--- src/Components/Aliens/AlienBase.cs
+++ src/Components/Aliens/AlienBase.cs
@@ -6,26 +6,21 @@ namespace SpaceInvadersRetro.Components.Aliens;
 
 public abstract class AlienBase : EntityBase
 {
-    protected Rectangle[] _sprites;
-    protected byte _spriteIdx = 0;
-    protected float _interval = 1f;
-    protected float _elapsedTime = 0f;
+    protected Rectangle[] Sprites;
+    protected byte SpriteIdx = 0;
+    private const float Interval = 1f;
+    protected float ElapsedTime = 0f;
 
     public abstract Texture2D Texture { get; set; }
     public abstract Vector2 Position { get; set; }
     public abstract bool IsAlive { get; set; }
     public abstract int Points { get; set; }
 
-    public void Animation()
+    protected void Animation()
     {
-        if (_elapsedTime > _interval)
-        {
-            if (_spriteIdx == 0)
-                _spriteIdx = 1;
-            else
-                _spriteIdx = 0;
+        if (!(ElapsedTime > Interval)) return;
+        SpriteIdx = SpriteIdx == 0 ? (byte)1 : (byte)0;
 
-            _elapsedTime = 0;
-        }
+        ElapsedTime = 0;
     }
 }
